0b0a747c83021322c07c99168431bc38a813771c
A7Aniket <aniketdha7@gmail.com>
2024-02-12 19:33:25 +0530

    updated cards

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -8,56 +8,32 @@ let productsHTML = "";
 
 products.forEach((product) => {
   productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
-
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars * 10}.png">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
-        </div>
-      </div>
-
-      <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
-      </div>
-
-      <div class="product-quantity-container">
-        <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id="${product.id}">
-        Add to Cart
-      </button>
+  <div class="product-container block rounded-lg p-4 shadow-sm shadow-indigo-100">
+    <div class="product-image-container">
+      <img
+        alt=""
+        src="${product.image}"
+        class="product-image h-56 w-full rounded-md object-cover"
+      />
     </div>
-  `;
+    <div class="mt-2">
+      <dl>
+        <div>
+          <dt class="sr-only">Price</dt>
+
+          <dd class="text-sm text-gray-500">$${(
+            product.priceCents / 100
+          ).toFixed(2)}</dd>
+        </div>
+
+        <div>
+          <dt class="sr-only">Product Name</dt>
+
+          <dd class="font-medium">${product.name}</dd>
+        </div>
+      </dl>
+    </div>
+  </div>`;
 });
 
 document.querySelector(".product-grid").innerHTML = productsHTML;
