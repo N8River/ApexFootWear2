b705dd6117c1a51aba2fd452a34d094204b40902
A7Aniket <aniketdha7@gmail.com>
2024-03-03 14:16:03 +0530

    payment container open and closes now

--- scripts/payment.js
+++ scripts/payment.js
@@ -3,3 +3,22 @@ const toggleButton = document.getElementById("toggleButton");
 toggleButton.addEventListener("click", () => {
   toggleButton.classList.toggle("active");
 });
+
+const checkoutBtn = document.querySelector(".checkout-btn");
+const paymentContainer = document.querySelector(".payment-container");
+const closeCheckoutBtn = document.querySelector(".close-checkout-btn");
+
+checkoutBtn.addEventListener("click", () => {
+  if (paymentContainer.classList.contains("active")) {
+    paymentContainer.classList.remove("active");
+    paymentContainer.classList.add("hide");
+  } else {
+    paymentContainer.classList.add("active");
+    paymentContainer.classList.remove("hide");
+  }
+});
+
+closeCheckoutBtn.addEventListener("click", () => {
+  paymentContainer.classList.remove("active");
+  paymentContainer.classList.add("hide");
+});
