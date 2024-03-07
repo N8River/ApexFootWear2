6fa23211cf68da9e0036a59bd2580c6e35224b7d
A7Aniket <aniketdha7@gmail.com>
2024-03-07 17:53:22 +0530

    footer responsive

--- scripts/common/footer.js
+++ scripts/common/footer.js
@@ -1,75 +1,35 @@
 document.querySelector(".footer-container").innerHTML = `
-<div class="footer-grid-1">
-        <h2 class="footer-title">VISIT STORE</h2>
-        <div class="store-location">
-          Sector 17, Chandigarh:
-          <div>
-            <a class="directions-link">Get Directions</a>
-          </div>
-        </div>
-      </div>
-      <div class="footer-grid-2">
-        <div class="footer-bullet">
-          <li class="li1">
-            <a href="about.html">About Us</a>
-          </li>
-          <li class="li1">
-            <a>FAQ</a>
-          </li>
-          <li class="li1">
-            <a>Blogs</a>
-          </li>
-        </div>
-      </div>
-      <div class="footer-grid-3">
-        <div class="footer-bullet">
-          <li class="li1">
-            <a>Track Order</a>
-          </li>
-          <li class="li1">
-            <a>Return/Replace</a>
-          </li>
-          <li class="li1">
-            <a>Returns/Replace Policy</a>
-          </li>
-        </div>
-      </div>
-      <div class="footer-grid-4">
-        <h2 class="footer-title">CUSTOMER CARE</h2>
-        <p class="p1">
-          Timings: 9 AM - 8 PM ( Mon - Sat ) <br />
-          Instagram: @apexftwear <br />
-          WhatsApp: +91 98765 43210
-        </p>
-      </div>
-      <div class="footer-signup">
-        <h2 class="footer-title">SIGN UP AND SAVE</h2>
-        <div>
-          <p class="p1">
-            Be the first to know about exclusive offers, latest trends & style
-            tips!
-          </p>
-        </div>
-
-        <div class="newsletter">
-          <input type="email" placeholder="Enter your email here" />
-          <button type="submit" class="newsletter-btn">
-            <img src="images/logos/svg/email.svg" />
-          </button>
-        </div>
-        <!--<div class="social-media-links">
-          <a>
-            <img src="images/logos/svg/socials/facebook.svg" />
-          </a>
-          <a>
-            <img src="images/logos/svg/socials/instagram.svg" />
-          </a>
-          <a>
-            <img src="images/logos/svg/socials/youtube.svg" />
-          </a>
-          <a>
-            <img src="images/logos/svg/socials/twitter.svg" />
-          </a>
-        </div>-->
-      </div>
+  <div class="footer-grid-1">
+    <h>VISIT STORE</h>
+    <p>Sector 17, Chandigarh:</p>
+    <p><a class="directions-link">Get Directions</a></p>
+  </div>
+  <div class="footer-grid-2">
+    <p><a href="about.html">About Us</a></p>
+    <p>FAQ</p>
+    <p>Blogs</p>
+  </div>
+  <div class="footer-grid-3">
+    <p>Track Order</p>
+    <p>Return/Replace</p>
+    <p>Returns/Replace Policy</p>
+  </div>
+  <div class="footer-grid-4">
+    <h>CUSTOMER CARE</h>
+    <p>Timings: 9 AM - 8 PM ( Mon - Sat )</p>
+    <p>Instagram: @apexftwear</p>
+    <p>WhatsApp: +91 98765 43210</p>
+  </div>
+  <div class="footer-signup">
+    <h>SIGN UP AND SAVE</h>
+    <p>
+      Be the first to know about exclusive offers, latest trends & style tips!
+    </p>
+    <div class="newsletter">
+      <input type="email" placeholder="Enter your email here" />
+      <button type="submit" class="newsletter-btn">
+        <img src="images/logos/svg/email.svg" />
+      </button>
+    </div>
+  </div>
 `;
