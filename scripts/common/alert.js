2690beaff560b6dd10c2772d55fa5df204345673
N8River <aniketdhiman145@gmail.com>
2024-03-21 19:54:59 +0530

    collection responsive

--- scripts/common/alert.js
+++ scripts/common/alert.js
@@ -4,3 +4,26 @@ export function alertCart() {
     document.querySelector(".cart-alert-wrap").classList.remove("active");
   }, 3500);
 }
+
+let lastScrollTop = 0;
+const rewardsPopUp = document.querySelector(".mobile-user");
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scrolling down
+
+    this.setTimeout(() => {
+      rewardsPopUp.style.display = "flex";
+    }, 1500);
+  } else {
+    // Scrolling up
+
+    this.setTimeout(() => {
+      rewardsPopUp.style.display = "flex";
+    }, 1500);
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
